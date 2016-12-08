f3b4a089cf45b4cdf0cbc9c51ec6e5ebeb938586
Suthaharan Tharmalingam <sutha@kurinchilion.com>
2016-12-07 23:20:00 -0500

    remove sqlite, add mysql connector, migrate db

--- newsblog/settings.py
+++ newsblog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'newsblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'feedblog',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
